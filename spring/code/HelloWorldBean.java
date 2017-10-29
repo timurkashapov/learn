00e4930e4cd82af4a8862de016eb73e925360dd0
Timur @shako Kashapov <timshako@gmail.com>
2017-10-29 22:29:01 +0300

    + add maven quickstart archetype#498

---
+++ spring/code/HelloWorldBean.java
@@ -0,0 +1,6 @@
+import javax.ejb.SessionBean;
+import javax.ejb.SessionContext;
+
+class HelloWorldBean implements SessionBean {
+
+}
---
+++ spring/code/SpringFrameworkLearning/src/test/java/com/shako/ApplicationTest.java
@@ -0,0 +1,11 @@
+package com.shako;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void testMainMethod() {
+
+    }
+}
