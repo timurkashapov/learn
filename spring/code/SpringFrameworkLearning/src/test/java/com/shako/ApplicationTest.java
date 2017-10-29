a8547fc7aca5580b159838299f85d44aac83752d
Timur @shako Kashapov <timshako@gmail.com>
2017-10-29 23:17:19 +0300

    add Mockito framework

--- spring/code/SpringFrameworkLearning/src/test/java/com/shako/ApplicationTest.java
+++ spring/code/SpringFrameworkLearning/src/test/java/com/shako/ApplicationTest.java
@@ -1,11 +1,14 @@
 package com.shako;
 
+import static org.mockito.Mockito.*;
 import org.junit.Test;
 
 public class ApplicationTest {
 
     @Test
     public void testMainMethod() {
+
+        mock(Class.class);
 
     }
 }
